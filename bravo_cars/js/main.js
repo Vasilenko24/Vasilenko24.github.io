eb902909ad1ffe4c1a72f80eaa4d39310a8ce536
Vasilenko24 <restylezp@gmail.com>
2020-06-02 13:35:33 +0300

    asdf

--- bravo_cars/js/main.js
+++ bravo_cars/js/main.js
@@ -1,75 +1,77 @@
 
 $(function(){
 
-  $('.header__lang').on('click', function(){
-    $('.header__lang, .lang__more').toggleClass('active');
-    });
-
-    $('.header__burger').on('click', function(){
-      $('.header__burger, .header__menu-content').toggleClass('active');
-      $('body').toggleClass('lock');
+    $('.header__lang').on('click', function(){
+      $('.header__lang, .lang__more').toggleClass('active');
       });
-
-
-$('.homeslider__inner').slick({
-  autoplay: true,
-  autoplaySpeed: 3000,
-  speed: 1500,
-  pauseOnFocus: true,
-  pauseOnDots: true,
-  pauseOnHover: true,
-  });
-
   
-  $('.offers__slider').slick({
+      $('.header__burger').on('click', function(){
+        $('.header__burger, .header__menu-content').toggleClass('active');
+        $('body').toggleClass('lock');
+        });
+  
+  
+  $('.homeslider__inner, .carslider__inner, .yachtsslider__inner').slick({
     autoplay: true,
     autoplaySpeed: 3000,
-    speed: 1500,
-    slidesToShow: 3,
-    arrows: true,
-    slidesToScroll: 1,
+    speed: 1000,
     pauseOnFocus: true,
     pauseOnDots: true,
     pauseOnHover: true,
+    focusOnSelect: false,
+    });
+  
     
-    responsive: [
-        {
-            breakpoint: 1185,
-            settings: {
-              slidesToShow: 2,
-            }
-          },
+    $('.offers__slider').slick({
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      speed: 1100,
+      slidesToShow: 3,
+      arrows: true,
+      slidesToScroll: 2,
+      pauseOnFocus: true,
+      pauseOnDots: true,
+      pauseOnHover: true,
+      
+      responsive: [
           {
-            breakpoint: 861,
-            settings: {
-              slidesToShow: 1,
-            }
-          },
-    ]
-});
-
-$('.filter').on('click', function(){
-  $(this).toggleClass('active');
+              breakpoint: 1185,
+              settings: {
+                slidesToShow: 2,
+              }
+            },
+            {
+              breakpoint: 861,
+              settings: {
+                slidesToShow: 1,
+              }
+            },
+      ]
+  });
+  
+  $('.filter').on('click', function(){
+    $(this).toggleClass('active');
+    
+    });
+  
+  $('body').append('<button class="btn__up" />');
+   
+  $('.btn__up').click(function(){
+    $('body').animate({'scrollTop' : 0 }, 1000);
+    $('html').animate({'scrollTop' : 0 }, 1000);
+  });
+  
+  $(window).scroll(function (){
+    if ($(this).scrollTop() > 200) {
+      $('.btn__up').addClass('active');
+   }  else {
+      $('.btn__up').removeClass('active');
+   };
+  });
+  
+  
+    $('input[type="file"], select').styler();
+  
   
   });
-
-$('body').append('<button class="btn__up" />');
- 
-$('.btn__up').click(function(){
-  $('body').animate({'scrollTop' : 0 }, 1000);
-  $('html').animate({'scrollTop' : 0 }, 1000);
-});
-
-$(window).scroll(function (){
-  if ($(this).scrollTop() > 200) {
-    $('.btn__up').addClass('active');
- }  else {
-    $('.btn__up').removeClass('active');
- };
-});
-
-
-  $('input[type="file"], select').styler();
-
-
-});
