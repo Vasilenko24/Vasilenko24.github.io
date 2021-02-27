7bad28f78b14adb6c7e1edf9691485005017a7e9
Vasilenko24 <64871285+Vasilenko24@users.noreply.github.com>
2021-02-27 02:09:28 +0200

    tet

--- food/script.js
+++ food/script.js
@@ -1,14 +1,14 @@
 require('es6-promise').polyfill();
 import 'nodelist-foreach-polyfill';
      
-     import tabs    from './modules/tabs';
-     import modal   from './modules/modal';
-     import timer   from './modules/timer';
-     import cards   from  './modules/cards';
-     import forms   from './modules/forms';
-     import calc    from  './modules/calc';
-     import slider from './modules/slider';
-     import {openModal} from './modules/modal';
+     import tabs    from './js/modules/tabs';
+     import modal   from './js/modules/modal';
+     import timer   from './js/modules/timer';
+     import cards   from  './js/modules/cards';
+     import forms   from './js/modules/forms';
+     import calc    from  './js/modules/calc';
+     import slider from './js/modules/slider';
+     import {openModal} from './js/modules/modal';
 
 
 window.addEventListener('DOMContentLoaded', () => {
