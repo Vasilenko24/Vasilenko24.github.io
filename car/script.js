d869b0effab9eaf60c59a3b197bf94c6efae68d9
Vasilenko24 <64871285+Vasilenko24@users.noreply.github.com>
2021-04-04 00:27:57 +0300

    gg

--- car/script.js
+++ car/script.js
@@ -93,7 +93,25 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
+const btn = document.getElementById("menu-toggle");
+const lines = btn.querySelectorAll(".line");
+const cls = {
+  open: "open",
+  close: "close"
+};
+let btnClass = cls.open;
+btn.addEventListener("click", () => {
+  if (btn.classList.contains(cls.open)) {
+    btn.classList.remove(btnClass);
+    btnClass = cls.close;
+  } else if (btn.classList.contains(cls.close)) {
+    btn.classList.remove(btnClass);
+    btnClass = cls.open;
+  }
 
+  void btn.offsetWidth;
+  btn.classList.add(btnClass);
+});
 
 /***/ })
 
